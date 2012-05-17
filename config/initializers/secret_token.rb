afbf15c6080d2b1f010f66a4e3d5fc7484cf04c0
seyyah <nurettin.senyer@bil.omu.edu.tr>
2012-05-17 07:21:11 +0300

    ilk

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TutorialAssociation::Application.config.secret_token = '694c8b1be5ed4a8146d8b73edc0c359b2bd8023b1fdcc9b04ad88a8212a50c5432a61dc750b584cb8970bbc3da3f16b21c7e1ef3f235780e324032fddb98134c'
